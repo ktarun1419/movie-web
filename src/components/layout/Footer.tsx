c13337d03e8c4a36af0491649b8ca11933c9aa3c
ktarun1419 <tarunkaushik355@gmail.com>
2024-02-06 19:14:02 +0530

    changes

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -74,9 +74,9 @@ export function Footer() {
           <p className="mt-3">{t("footer.legal.disclaimerText")}</p>
         </div>
         <div className="flex flex-wrap gap-[0.5rem] -ml-3">
-          <FooterLink icon={Icons.GITHUB} href={conf().GITHUB_LINK}>
+          {/* <FooterLink icon={Icons.GITHUB} href={conf().GITHUB_LINK}>
             {t("footer.links.github")}
-          </FooterLink>
+          </FooterLink> */}
           <FooterLink icon={Icons.DISCORD} href={conf().DISCORD_LINK}>
             {t("footer.links.discord")}
           </FooterLink>
